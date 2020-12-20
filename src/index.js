88efe0a8cc014ba9c5fc34e2954c8a3551a2beff
Damika Anupama <damikaanupama@gmail.com>
2020-12-20 18:29:31 +0530

    Add manage-items place orders search-orders settings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import dashboard from './dashboard/dashboard.html';
 import managecustomer from './manage-customer/manage-customer.html';
+import manageitem from './manage-items/manage-item.html';
+import placeorder from './place-orders/place-orders.html';
+import searchorder from './search-orders/search-orders.html';
+import settings from './settings/settings.html';
 
-$('#container').append(dashboard,managecustomer);
+$('#container').append(dashboard,managecustomer,manageitem,placeorder,searchorder,settings);
 
 import '../node_modules/admin-lte/plugins/jquery/jquery.min.js';
 import '../node_modules/admin-lte/plugins/jquery-ui/jquery-ui.min.js';
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,27 @@
-$("#main-menu a").click(function(){
+$("#main-menu a").click(function () {
     $("#main-menu a").removeClass("active");
-     $(this).addClass("active");
-     $(".content-wrapper").addClass("d-none");
-     switch ($(this).attr("data-menu")) {
+    $(this).addClass("active");
+    $(".content-wrapper").addClass("d-none");
+    switch ($(this).attr("data-menu")) {
         case "DASHBOARD" :
             $("#dashboard").removeClass("d-none");
             break;
         case "MANAGE-CUSTOMERS":
             $("#manage-customers").removeClass("d-none");
             break;
+        case "MANAGE-ITEMS":
+            $("#manage-items").removeClass("d-none");
+            break;
+        case "PLACE-ORDERS":
+            $("#place-orders").removeClass("d-none");
+            break;
+        case "SEARCH-ORDER":
+            $("#search-orders").removeClass("d-none");
+            break;
+        case "SETTINGS":
+            $("#settings").removeClass("d-none");
+            break;
         default:
             break;
-     }
+    }
 });
